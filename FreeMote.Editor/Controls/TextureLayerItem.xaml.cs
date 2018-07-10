f00ca9cd3fb42ed306923ad17e5601b0e2502ab4
UlyssesWu <wdwxy12345@gmail.com>
2018-07-10 10:24:38 +0800

    Init UI

---
+++ FreeMote.Editor/Controls/TextureLayerItem.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FreeMote.Editor.Annotations;
+
+namespace FreeMote.Editor.Controls
+{
+    /// <summary>
+    /// TextureLayerItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class TextureLayerItem : UserControl, INotifyPropertyChanged
+    {
+        public TextureLayerItem()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ FreeMote.Editor/Models/PsbViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using FreeMote.Editor.Annotations;
+using FreeMote.Psb;
+
+namespace FreeMote.Editor.Models
+{
+    class PsbViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public PSB Model { get; set; }
+        internal PsbPainter Painter { get; set; }
+
+        public void Update()
+        { }
+
+        public bool ReplaceTexture()
+        {
+            return true;
+        }
+    }
+}
